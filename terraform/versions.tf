2e708ded5bb63549835c6d202efbab0e308eddfa
zaki3mymy <zaki.last@gmail.com>
2025-08-21 15:26:14 +0000

    tfsafeをS3に保存する

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.2.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.92"
     }
+  }
+
+  # `terraform init`のときに`-backend-config`で以下の値を設定する
+  # -backend-config="bucket=<BUCKET_NAME>"
+  # -backend-config="key=shopping-reminder/terraform.tfstate"
+  # -backend-config="region=ap-northeast-1"
+  backend "s3" {
   }
 }
